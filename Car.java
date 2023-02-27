30e0a7d1f09b6ebf91ad89fc7999e760fa993bcf
ReyolRhema <126499554+ReyolRhema@users.noreply.github.com>
2023-02-27 22:19:27 +0530

    Add files via upload

---
+++ Car.java
@@ -0,0 +1,37 @@
+package week2day1;
+
+public class Car 
+{ 
+public void engine() {
+    	System.out.println("engine");
+    }
+    public void colour() {
+    	System.out.println("colour");
+    }
+    public void fourwheels() {
+    	System.out.println("fourwheels");
+    }
+    public void fiveseater() {
+    	System.out.println("fiveseater");
+    }
+    public void sunroof() {
+    	System.out.println("sunroof");
+    }
+public static void main(String[] args) {
+    	Car mycarss=new Car();
+    	mycarss.engine();
+    	mycarss.colour();
+    	mycarss.fourwheels();
+    	mycarss.fiveseater();
+    	mycarss.sunroof();
+    }
+}
+
+
+    	
+    	
+    
+    
+    
+		
+	
---
+++ Mobile.java
@@ -0,0 +1,32 @@
+package week2day1;
+
+public class Mobile
+{
+   public void display() {
+	   System.out.println("display");
+   }
+     public void backcase() {
+	   System.out.println("backcase");
+     }
+	   public void simcard() {
+		   System.out.println("simcard");
+	   }
+	   public void recharge() {
+		   System.out.println("recharge");
+	   }
+	   public void keypad() {
+		   System.out.println("keypad");
+	   }
+public static void main(String[] args) {
+	Mobile Mymobiles=new Mobile();
+	Mymobiles.display();
+	Mymobiles.backcase();
+	Mymobiles.simcard();
+	Mymobiles.recharge();
+	Mymobiles.keypad();
+}
+}
+
+
+	   
+
